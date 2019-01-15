f4baea5c38f0ab086b663adc24972d5680fd6d62
HarringtonR <ross.ludeke@gmail.com>
2019-01-15 00:54:35 -0600

    restart

--- App.js
+++ App.js
@@ -1,22 +1,49 @@
-import React from 'react';
-import Loading from './components/Loading';
-import Home from './components/Home';
-import {
-      createStackNavigator,
-      createAppContainer
-    } from 'react-navigation';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-const RootStack = createStackNavigator({
-    Loading: {
-      screen: Loading
-    },
-    Home: {
-      screen: Home
-    }
-  });
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const App = createAppContainer(RootStack);
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
 
-export default App;
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    );
+  }
+}
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
